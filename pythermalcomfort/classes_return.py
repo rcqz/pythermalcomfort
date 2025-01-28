2e3c54091a230b96dbf3d2cc2fcd43b5ae1d9b4f
t-kramer <t.kramer@berkeley.edu>
2025-01-28 13:37:10 -0800

    build: implement dataclasses for adaptive charts
    Commit covers a first implementation of dataclasses for adaptive charts.
    While not completely implemented, I think the idea is clear. Based on
    review we can implement a similar structure for the other charts.
    
    The following additional, minor changes were made:
    - implemented return of stress_category for heat_index_rothfusz
    - added >= 27 °C criterion for heat_index_rothfusz, returns NaN if not met
    - adjusted plot_heatmap_hi accordingly
    - slight modifications to application of comfort models to dataframes:
    conversion to np.array to longer needed

--- pythermalcomfort/classes_return.py
+++ pythermalcomfort/classes_return.py
@@ -227,9 +227,12 @@ class HI(AutoStrMixin):
     ----------
     hi : float or list of floats
         Heat Index, [°C] or [°F] depending on the units.
+    stress_category : str or list of strs
+        Heat Index categorized in terms of heat stress.
     """
 
     hi: Union[float, list[float]]
+    stress_category: Union[str, list[str]]
 
 
 @dataclass(frozen=True, repr=False)
